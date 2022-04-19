555a358a6c8f2e4e1d7330c229ee4342c5846eaf
Welnay <103646118+Welnay@users.noreply.github.com>
2022-04-19 18:36:13 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,23 +1,19 @@
-const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { DiscordAPIError } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commands = [
+	new SlashCommandBuilder()
+		.setName('menu')
+		.setDescription('Permet d\'ouvrir un menu de sélection.')
+]
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
+	.map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-(async () => {
-    try {
-        await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
-        console.log('Les commandes ont étés enregistrées !')
-    } catch (error) {
-        console.error(error);
-    }
-})();
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Deploy Commands actualisé.'))
+	.catch(console.error);
