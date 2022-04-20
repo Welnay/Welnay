168f64e3cb710c7e8ed72871f9f902cf2f553a58
Welnay <103646118+Welnay@users.noreply.github.com>
2022-04-20 16:49:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const wait = require('node:timers/promises').setTimeout;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING ] });
 
 client.once('ready', () => {
-	console.log('WellBot démarré');
+	console.log('WellBot est en ligne !');
 	client.user.setStatus('dnd');
     const guild = client.guilds.cache.get(guildId);
     const memberCount = guild.memberCount
@@ -23,8 +23,8 @@ client.once('ready', () => {
 	  }, 10000);
 });
 
-client.on('interactionCreate', interaction => {
-	if (!interaction.isCommand()) return;
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand() || !interaction.member.permissions.has('ADMINISTRATOR')) return await interaction.reply({ content: 'Vous n\'avez pas accès à cette commande.', ephemeral: true });
 	
 	const { commandName } = interaction;
 	const message = Message;
